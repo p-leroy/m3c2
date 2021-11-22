f9adb93de5e53afeca8eb58bd52dc9350fede707
Paul Leroy <paul.leroy@univ-rennes1.fr>
2021-11-22 23:06:27 +0100

    wip

---
+++ data_iris.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 22 15:31:22 2021
+
+@author: PaulLeroy
+"""
+
+import os
+
+path1 = '//lidar-server/DATA/RENNES1/New_Zealand/Zone_B/2017/Donnees_brutes/assemblage_1'
+
+os.listdir(path1)
